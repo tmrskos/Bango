cd9762c992f78d98785d5ac5bb9718d12c0f666d
Todd <tmrskos@hotmail.com>
2020-03-24 13:22:29 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("Hello dude");
+        System.out.println("Hello dude-from git");
     }
 }
